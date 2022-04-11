1d4ee0dbc1455e0af4e1283606d21d32123b5592
gialipo84 <97461693+gialipo84@users.noreply.github.com>
2022-04-11 13:55:17 +0400

    საბოლოო

---
+++ css-folder/file.js
@@ -0,0 +1,6 @@
+let navBar=document.getElementById('ul-nav-id');
+let toggleButton=document.getElementById('burger-button');
+toggleButton.addEventListener('click', function(){
+    navBar.classList.toggle('active-ul')
+          
+})
